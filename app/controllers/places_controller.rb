769523b7c1a5ed8948dee4a86d79ac233aee0b17
rob_alef <ralef7@uchicago.edu>
2015-07-09 15:59:54 -0500

    through most parts of first 2 challanges working on delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,13 @@ class PlacesController < ApplicationController
 	def index
 		@places = Place.all
 	end
+
+	def show
+		@place = Place.find_by(:id => params["id"])
+	end
+
+	def delete
+		Place.destroy(:id => params["id"])
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   root 'places#index'
-  
+
   get  '/places' => 'places#index'
+
+  get '/places/:id' => 'places#show'
+
+  get '/places/:id/delete' => 'places#delete'
 
 end
