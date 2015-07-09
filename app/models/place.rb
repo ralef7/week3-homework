7b884bf5bd444297659233c48052d57cef9600ab
rob_alef <ralef7@uchicago.edu>
2015-07-09 14:31:44 -0500

    database working with website

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
 
-	attr_accessor :title, :admission_price, :description, :photo_url
+#	attr_accessor :title, :admission_price, :description, :photo_url
 
 	# def initialize(the_title, the_admission_price, the_description, the_photo_url)
 	#     self.title = the_title
